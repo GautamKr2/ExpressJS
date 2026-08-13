0accd7a7c164f44973d90cebadd45a793fc71e68
GautamKr2 <gautamkumar11008@gmail.com>
2026-08-13 19:58:04 +0530

    Rendering CSS file in Node JS

--- htmlFileRender.js
+++ htmlFileRender.js
@@ -4,6 +4,9 @@ import path from 'path';
 const app = express();
 let absPath = path.resolve('html')
 
+let publicPath = path.resolve('public')
+app.use(express.static(publicPath))
+
 app.get("/", (req, resp) => {
     // let absPath = path.resolve('html/home.html');
     resp.sendFile(absPath+"/home.html")
