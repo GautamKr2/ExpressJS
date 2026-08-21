74cfc16d80216ea2bca44b425c87ba82c703a952
GautamKr2 <gautamkumar11008@gmail.com>
2026-08-22 00:42:00 +0530

    Loops, condition in ejs

--- ejs.js
+++ ejs.js
@@ -2,9 +2,23 @@ import express from 'express';
 const app = express();
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded())
 
 app.get("/", (req, resp) => {
-    resp.render("home")
+    resp.render("home", {name: 'Gautam Kumar', age: 22})
+});
+
+app.get("/add-user", (req, resp) => {
+    resp.render("addUser")
+});
+
+app.post("/submit-user", (req, resp) => {
+    resp.render('submitUser', req.body)
+});
+
+app.get("/users", (req, resp) => {
+    const users = ['Gautam', 'Amit', 'Suman', 'Sashi', 'Ravi']
+    resp.render('users', {user:users, isLogin:true, age:23})
 })
 
 app.listen(3400)
