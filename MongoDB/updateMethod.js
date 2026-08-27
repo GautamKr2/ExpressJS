538cfcb2b4bb75cf24a59883eff005039d2c552d
GautamKr2 <gautamkumar11008@gmail.com>
2026-08-28 01:04:58 +0530

    Update data with PUT method

--- MongoDB/updateMethod.js
+++ MongoDB/updateMethod.js
@@ -6,6 +6,9 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views', 'MongoDB/views')
 
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
 const dbName = 'college';
 const client = new MongoClient('mongodb://localhost:27017');
 
@@ -18,10 +21,37 @@ client.connect().then((connection) => {
         resp.render('studentsData', {studentsData});
     })
 
+    // Rendering the form
     app.get("/update/:id", async (req, resp) => {
         let id = req.params.id;
         let stdData = await collection.findOne({_id: new ObjectId(id)});
         resp.render('update-student', {stdData});
+    })
+
+    // Updating the data through UI
+    app.post("/ui/update/:id", async (req, resp) => {
+        let id = req.params.id;
+        let update = {$set: req.body}
+        let result = await collection.updateOne({_id: new ObjectId(id)}, update);
+        if(result.modifiedCount == 1) {
+            resp.send("<h1> Data updated </h1>")
+        }
+        else {
+            resp.send("<h1> Data not updated </h1>")
+        }
+    })
+
+    // Updating the data from API
+    app.put("/update/:id", async (req, resp) => {
+        let id = req.params.id;
+        let update = {$set: req.body}
+        let result = await collection.updateOne({_id: new ObjectId(id)}, update);
+        if(result.modifiedCount == 1) {
+            resp.send({message: "data updated", success: true, result: result})
+        }
+        else {
+            resp.send({message: "Data not updated", success: false, result: result})
+        }
     })
 })
 
