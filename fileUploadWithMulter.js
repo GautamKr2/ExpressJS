58d45b28138cb505240e5c3da8b192aa227c28f9
GautamKr2 <gautamkumar11008@gmail.com>
2026-08-28 16:17:41 +0530

    Upload file using multer package

--- fileUploadWithMulter.js
+++ fileUploadWithMulter.js
@@ -1,20 +1,35 @@
 import express from 'express';
+import multer from 'multer';
+
 const app = express();
+
+// const upload = multer({dest: 'upload'});
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({storage});
 
 app.get("/", (req, resp) => {
     resp.send(`
-        <form action="/upload" method="post" enctype="multipart/info-data/">
+        <form action="/upload" method="post" enctype="multipart/form-data">
             <input type="file" name="myFile" />
             <button> Upload file </button>
         </form>
     `)
 })
 
-app.post("/upload", (req,resp) => {
+app.post("/upload", upload.single('myFile'), (req,resp) => {
     resp.send({
         message: "File Uploaded",
-        info: null
+        info: req.file
     })
 })
 
-app.listen(3200)
+app.listen(3200);
