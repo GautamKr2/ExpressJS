9a31cb7fc480435758f9b35fd62553fbcd9c20f7
GautamKr2 <gautamkumar11008@gmail.com>
2026-08-29 15:33:53 +0530

    File upload with multer

--- fileUploadWithMulter.js
+++ fileUploadWithMulter.js
@@ -3,8 +3,6 @@ import multer from 'multer';
 
 const app = express();
 
-// const upload = multer({dest: 'upload'});
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'upload')
@@ -14,6 +12,7 @@ const storage = multer.diskStorage({
     }
 })
 
+// const upload = multer({dest: 'upload'});
 const upload = multer({storage});
 
 app.get("/", (req, resp) => {
